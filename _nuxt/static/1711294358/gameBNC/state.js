56f91b37eb4cbfb0166199f8e38c762ebdf232b1
ellen9527 <ellen9527@users.noreply.github.com>
2024-03-24 16:13:48 +0000

    deploy: 21039ea5155388e113f8bd19a18d65c4e5f7c9d0

--- _nuxt/static/1711294358/gameBNC/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fgames\u002F_nuxt\u002Fstatic\u002F1711294358",layout:"default",error:a,serverRendered:true,routePath:"\u002FgameBNC",config:{_app:{basePath:"\u002Fgames\u002F",assetsPath:"\u002Fgames\u002F_nuxt\u002F",cdnURL:a}}}}(null));
---
+++ _nuxt/static/1711296822/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002FgameBNC","\u002Flist","\u002Finspire","\u002Fjiugongge"]})
